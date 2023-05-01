5aa08fb0cb4a78c7f5456ef7f57d817acaf8f69f
RobertoHiro <mrhiroshiroberto@yahoo.com.br>
2023-05-01 13:59:11 -0300

    primeiro git

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,31 @@
 import { Component } from '@angular/core';
+// import { User } from './class/user';
+import { User } from './class/user/user.component';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.scss']
 })
+
 export class AppComponent {
   title = 'Colegio-Vencer-Sempre';
+  localStorage = window.localStorage;
+  hasUser = false;
+  localUser:string|null;
+
+  constructor(private router: Router){
+    this.localUser = this.localStorage.getItem("user");
+    console.log(this.localUser);
+  }
+  ngOnInit(){
+    if (this.localUser) {
+      this.hasUser = true;
+      this.router.navigateByUrl("/main");
+    }
+    else{
+      this.router.navigateByUrl("/login");
+    }
+  }
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,18 +1,34 @@
 import { NgModule } from '@angular/core';
+import { RouterModule, Routes } from '@angular/router';
 import { BrowserModule } from '@angular/platform-browser';
-
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 
+import { LoginComponent } from './login/login.component';
+import { MainComponent } from './main/main.component';
+// import { User } from './class/user';
+import { UserComponent } from './class/user/user.component';
+
+const routes: Routes = [
+  {path: '', component: AppComponent},
+  {path: 'login', component: LoginComponent},
+  {path: 'main', component: MainComponent},
+]
+
 @NgModule({
-  declarations: [
-    AppComponent
-  ],
   imports: [
     BrowserModule,
+    RouterModule.forRoot(routes),
     AppRoutingModule
   ],
-  providers: [],
+  declarations: [
+    AppComponent,
+    LoginComponent,
+    MainComponent,
+    UserComponent
+  ],
+  providers:[],
   bootstrap: [AppComponent]
 })
+
 export class AppModule { }
